bc22516891a45ecdf2184d89d0bce680543649a8
DiegoAvmor <diegoavmor@gmail.com>
2021-09-27 14:42:22 -0500

    🗃️ Added initial database schema creation

---
+++ src/main/resources/db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE  IF NOT EXISTS `smart_entertainment_api`;
+USE `smart_entertainment_api`;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
