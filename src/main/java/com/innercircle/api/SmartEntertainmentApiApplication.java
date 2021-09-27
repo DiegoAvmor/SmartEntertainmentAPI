a688622fe08fb224f78f85956c8e9fb6ec32af27
DiegoAvmor <diegoavmor@gmail.com>
2021-09-27 14:30:47 -0500

    🎉 Created Base Project

---
+++ src/main/java/com/innercircle/api/SmartEntertainmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.innercircle.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEntertainmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEntertainmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innercircle/api/SmartEntertainmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innercircle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEntertainmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
