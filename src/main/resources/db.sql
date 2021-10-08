5f0f8f110daac4f556639f7dc1cfc3b8fd6132b0
DiegoAvmor <diegoavmor@gmail.com>
2021-10-08 09:59:20 -0500

    Added database schema file

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,48 @@
-CREATE DATABASE  IF NOT EXISTS `smart_entertainment_api`;
+DROP SCHEMA IF EXISTS `smart_entertainment_api`;
+CREATE SCHEMA IF NOT EXISTS `smart_entertainment_api`;
 USE `smart_entertainment_api`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE `users` (
+  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `email` varchar(255),
+  `password` varchar(255)
+);
+
+CREATE TABLE `resource` (
+  `resource_id` int PRIMARY KEY AUTO_INCREMENT,
+  `resource_type` int,
+  `image_url` varchar(255),
+  `name` varchar(50),
+  `description` varchar(200),
+  `year_publish` int,
+  `num_chapter` int
+);
+
+CREATE TABLE `resource_types` (
+  `resoucer_type_id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255)
+);
+
+CREATE TABLE `user_resources` (
+  `user_id` int,
+  `resource_id` int,
+  `status_id` int,
+  `date_started` timestamp,
+  `date_finished` timestamp,
+  `is_favorite` bool,
+  `current_chapter` int
+);
+
+CREATE TABLE `status` (
+  `status_id` int PRIMARY KEY AUTO_INCREMENT,
+  `status_name` varchar(15)
+);
+
+ALTER TABLE `resource` ADD FOREIGN KEY (`resource_type`) REFERENCES `resource_types` (`resoucer_type_id`);
+
+ALTER TABLE `user_resources` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `user_resources` ADD FOREIGN KEY (`resource_id`) REFERENCES `resource` (`resource_id`);
+
+ALTER TABLE `user_resources` ADD FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`);
